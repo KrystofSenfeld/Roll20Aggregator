5a1cc3b904c3a285db3a69f0e981a8096f953e1b
Krystof Senfeld <ksenfeld@hotmail.com>
2022-11-21 21:45:45 +0000

    Installing Radzen package

--- PracticeBlazorApp/Program.cs
+++ PracticeBlazorApp/Program.cs
@@ -1,12 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Roll20Aggregator.Services;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Roll20Aggregator {
