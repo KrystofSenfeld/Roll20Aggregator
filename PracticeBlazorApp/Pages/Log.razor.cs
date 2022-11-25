d33ad9643a46be58098fefbcd3ef3dfa599223ae
Krystof Senfeld <krystof.senfeld@codify.ltd.uk>
2022-11-25 09:03:02 +0000

    Created log page for testing.

---
+++ PracticeBlazorApp/Pages/Log.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+using Roll20Aggregator.Services;
+
+namespace Roll20Aggregator.Pages {
+    [Route("/log")]
+    public partial class Log {
+        [Inject] ParsingSession ParsingSession { get; set; }
+    }
+}
